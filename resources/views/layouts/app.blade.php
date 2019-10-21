a1e3d8faa9b0ce7efb72ff88a31d7003911b0670
Elma Suljic <elmasuljic91@gmail.com>
2019-10-21 07:47:38 +0200

    Downloaded jquery and bootstrap to local

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
+	<script src="{{ asset('public/assets/js/jquery-3.3.1.min.js') }}" ></script>
 	<script src="{{ asset('public/assets/js/toastr.min.js') }}" ></script>
 	<script src="{{ asset('public/assets/js/sweetalert.min.js') }}" ></script>
     <!-- Fonts -->
@@ -20,7 +20,7 @@
     <link href="{{ asset('public/assets/css/app.css') }}" rel="stylesheet"> 
 	<link href="{{ asset('public/assets/css/toastr.css') }}" rel="stylesheet">
 	<link rel="stylesheet" href="{{ url('/public/assets/fonts/fontawesome/css/all.css') }}">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{ asset('public/assets/css/bootstrap.min.css') }}" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
 </head>
 <body>
@@ -99,8 +99,8 @@
         </main>
     </div>
 	<script src="{{ asset('public/assets/js/wow.min.js') }}" ></script>	
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="{{ asset('public/assets/js/popper.min.js') }}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="{{ asset('public/assets/js/js/bootstrap.min.js') }}" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script type="text/javascript">
 		new WOW().init();
 		
